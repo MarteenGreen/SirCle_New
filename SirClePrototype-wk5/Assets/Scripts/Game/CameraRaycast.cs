6bf68a2ae50690365442dac15f643fbbdd63bbff
MarteenGreen <MarteenRodriguez@gmail.com>
2016-05-05 12:07:50 -0700

    Camera Raycast v1
    When the player goes behind an object, the object between him and the
    camera turns invisible.
    (Not yet completed. I still need to turn them back on when player leaves
    the area.)

---
+++ SirClePrototype-wk5/Assets/Scripts/Game/CameraRaycast.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRaycast : MonoBehaviour 
+{
+    Camera camera;
+
+    public GameObject toHide;
+    public bool turnInvisible = false;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();     
+    }
+
+    void Update()
+    {
+        Ray ray = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        { 
+            // make this object transparent if NOT player
+            if (hit.transform.tag == "ground")
+            {
+                toHide = hit.transform.gameObject;
+                toHide.GetComponent<MeshRenderer>().enabled = false;
+                turnInvisible = true;
+            }
+        }
+        
+    }
+}
