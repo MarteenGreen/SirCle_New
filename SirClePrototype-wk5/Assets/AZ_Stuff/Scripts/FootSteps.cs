d9a208aa6607028e3cfa966fa9e5912b30c040c9
CroBobi <andrijazoric@Andrijas-MacBook-Air.local>
2016-05-05 10:34:29 -0700

    AzStuff soUNDS AND sCRPITS
    stuff we need for sound

---
+++ SirClePrototype-wk5/Assets/AZ_Stuff/Scripts/FootSteps.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(AudioSource))]
+
+public class FootSteps : MonoBehaviour {
+	
+	public AudioClip Footstep;
+	private AudioSource footSteps;
+	private float movementDistance;
+
+	private float volLowRange = .5f;
+	private float volHighRange = 0.8f;
+	
+	private float lowPitchRange = .75F;
+	private float highPitchRange = 1.0F;
+	void Awake()
+	{
+		movementDistance = 0.0f;
+		footSteps = GetComponent<AudioSource>();
+	}
+	
+	void Start ()
+	{
+	}
+	
+	
+	void Update ()
+	{
+
+		if(Input.GetKey ("w") || Input.GetKey ("a") || Input.GetKey ("s") || Input.GetKey ("d"))
+		{
+			movementDistance += 0.1f;
+
+		}
+		
+		if (movementDistance >= 1.3f)
+		{
+			footSteps.pitch = Random.Range (lowPitchRange,highPitchRange);
+			float vol = Random.Range (volLowRange, volHighRange);
+			footSteps.PlayOneShot(Footstep,vol);
+			movementDistance = 0.0f;
+		}
+		
+	}
+}
---
+++ SirClePrototype-wk5/Assets/AZ_Stuff/Scripts/Music.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+//CODE IS SHIT, NEEDS MEGA CLEANUP, IGNORE FOR NOW(AT LEAST IT WORKS)
+public class Music : MonoBehaviour {
+
+	public ThemesScript Themes;
+			
+	// Use this for initialization
+	void Start () {
+
+		Themes = FindObjectOfType<ThemesScript>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		
+		if (col.gameObject.tag == "Jump") {
+			if (Themes.Test == 0) {
+				VolumeControl (Themes.Source, 1);
+			} else if (Themes.Test == 1) {
+				VolumeControl (Themes.Sources1, 1);
+			}
+	
+
+		} else if (col.gameObject.tag == "Water") {
+			if (Themes.Test == 0) {
+				VolumeControl (Themes.Source, 2);
+			}
+			/*else if (Themes.Test ==1)
+			{
+				VolumeControl(Themes.Sources1,2);
+			}*/
+		} else if (col.gameObject.tag == "Change") {
+			Themes.Test = 1;
+			Themes.playing=false;
+		}
+	}
+  public void VolumeControl(AudioSource[] SourceA,int Piece)
+	{
+		SourceA[Piece].volume = 0.4f;
+	}
+}
