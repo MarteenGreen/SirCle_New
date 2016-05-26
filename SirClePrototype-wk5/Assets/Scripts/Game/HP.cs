ed262e0dbd09fdb55cde1d454080806e1f1059bb
Chinchilla03 <Chinchilla03@gmail.com>
2016-05-26 01:12:20 -0700

    AIGoodStuff
    Canon, Basic Goon, and IFRAMES

---
+++ SirClePrototype-wk5/Assets/Scripts/Game/HP.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HP : MonoBehaviour {
+    [SerializeField]
+    private int health;
+
+	// Use this for initialization
+	void Start () {
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void HPMinus(int dmg)
+    {
+        health -= dmg;
+    }
+    public void HPSet(int newHP)
+    {
+        health = newHP;
+    }
+    public int GetHP()
+    {
+        return health;
+    }
+}
