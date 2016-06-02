f1fa751209cb39338f8e771e5ed2b448e6049de5
MarteenGreen <MarteenRodriguez@gmail.com>
2016-06-01 21:13:40 -0700

    Camera Raycast v3
    Additional comments, hopefully this goes thru.

--- SirClePrototype-wk5/Assets/Scripts/Game/CameraRaycast.cs
+++ SirClePrototype-wk5/Assets/Scripts/Game/CameraRaycast.cs
@@ -8,15 +8,19 @@ public class CameraRaycast : MonoBehaviour
     public GameObject toHide;
     public bool turnInvisible = false;
 
+	private float alphaObject;
+
     void Start()
     {
-        camera = GetComponent<Camera>();     
+        camera = GetComponent<Camera>();
+		alphaObject = toHide.GetComponent<MeshRenderer>().material.color.a;
     }
 
     void Update()
     {
         Ray ray = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
+
         if (Physics.Raycast(ray, out hit))
         { 
             // make this object transparent if NOT player
@@ -25,8 +29,9 @@ public class CameraRaycast : MonoBehaviour
                 toHide = hit.transform.gameObject;
                 toHide.GetComponent<MeshRenderer>().enabled = false;
                 turnInvisible = true;
-            }
+			}
 
+			// Once the player is visible by the camera again, turn the object back on.
             if (toHide.GetComponent<MeshRenderer>().enabled == false && hit.transform.tag == "Player")
             {
                 toHide.GetComponent<MeshRenderer>().enabled = true;
