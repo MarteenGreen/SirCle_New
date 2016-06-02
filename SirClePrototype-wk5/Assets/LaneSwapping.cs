549281489737da725170fdeafd970c8b69500e0a
MarteenGreen <student@AICASF.EDMC.STU>
2016-06-02 11:58:11 -0700

    Changes week 9
    Things I added/changed:
    - Added an under-shadow to the player (using a Projector prefab)
    - Added the ammo system, and click audio queue when player tries to fire
    ammo but is empty.
    - Added the dodge roll that allows player to roll thru 2 lanes at once.
    - Made player obstructing objects turn invisible and visible depending
    on if the camera can see the player.
    TO DO:
    - Add a slow alpha transparency so objects don't suddenly dissappear,
    rather have them fade into invisibilty.
    - Lerp the lane swapping

--- SirClePrototype-wk5/Assets/LaneSwapping.cs
+++ SirClePrototype-wk5/Assets/LaneSwapping.cs
@@ -7,10 +7,14 @@ public class LaneSwapping : MonoBehaviour {
 	public GameObject player;
 
 	private Vector3 tmpPos;
+    
+    private bool canDodge;
+    public float WaitBeforeDodging = 1f;
+    public float CooldownDodgeTime = 1f;
 
 	// Use this for initialization
 	void Start () {
-	
+        canDodge = true;
 	}
 	
 	// Update is called once per frame
@@ -24,11 +28,28 @@ public class LaneSwapping : MonoBehaviour {
 			{
 				LaneSwapAway();
 			}
-
 			if(Input.GetKeyDown(KeyCode.S) && PlayerZChecks.isHittingTowards == false)
 			{
 				LaneSwapTowards();
 			}
+
+            // roll dodge thru 2 lanes at once (while holding shift and pressing a direction), with a cooldown (to prevent dodge roll spamming)
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.W) && canDodge)
+            {
+                LaneDodgeAway();
+                //Now start a coroutine that will wait before dodging
+                StartCoroutine(WaitAndDodge(WaitBeforeDodging));
+                canDodge = false;
+                StartCoroutine(CoolDown(CooldownDodgeTime));	
+            }
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.S) && canDodge)
+            {
+                LaneDodgeTowards();
+                //Now start a coroutine that will wait before dodging
+                StartCoroutine(WaitAndDodge(WaitBeforeDodging));
+                canDodge = false;
+                StartCoroutine(CoolDown(CooldownDodgeTime));
+            }
 		}
 	
 	}
@@ -39,10 +60,10 @@ public class LaneSwapping : MonoBehaviour {
 		if (tmpPos.z < maxZDepth)
 		{
 			tmpPos.z = tmpPos.z + 1;    	// assign individual Z axes
+            // tmpPos.z =  Mathf.Lerp(tmpPos.z, 1f, 0.5f); // only lerps everytime the key is pressed. will change.
 			transform.position = tmpPos;   // Assign back all Vector3
 		}
 	}
-
 	// when a character needs to swap to a lane *towards* the camera
 	public void LaneSwapTowards()
 	{
@@ -52,4 +73,34 @@ public class LaneSwapping : MonoBehaviour {
 			transform.position = tmpPos;   // Assign back all Vector3
 		}
 	}
+
+    // Dodge roll through 2 lanes away from the camera at once, instead of 1
+    public void LaneDodgeAway()
+    {
+        if (tmpPos.z < maxZDepth)
+        {
+            tmpPos.z = tmpPos.z + 2;    	// assign individual Z axes
+            transform.position = tmpPos;   // Assign back all Vector3
+        }
+    }
+    public void LaneDodgeTowards()
+    {
+        if (tmpPos.z > 0)
+        {
+            tmpPos.z = tmpPos.z - 2;    	// assign individual Z axes
+            transform.position = tmpPos;   // Assign back all Vector3
+        }
+    }
+
+    IEnumerator WaitAndDodge(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+    }
+
+    // Stops the player from spamming dodges
+    IEnumerator CoolDown(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        canDodge = true;
+    }
 }
--- SirClePrototype-wk5/Assets/Scripts/Player/RangedAttack.cs
+++ SirClePrototype-wk5/Assets/Scripts/Player/RangedAttack.cs
@@ -9,12 +9,16 @@ public class RangedAttack : MonoBehaviour {
 	public GameObject ProjectileObject;
 	public Transform SpawnPosition;
 
+    public AudioClip noAmmo;
+
 	public float WaitBeforeFiring =0.2f;
 	public float CooldownTime =1f;
 
 	private GameObject LastFiredProjectile;
 	
 	private bool CanShoot;
+
+    public int bulletCount = 3;
 
 	public float leftOrRight;
 
@@ -29,6 +33,7 @@ public class RangedAttack : MonoBehaviour {
 	void Update () {
 
 		// Binary shooting directions, either just left or right. based on how much he's facing in the given direction.
+        // NOTE: Currently broken-- can only shoot towards the right side of the screen
 		if(this.gameObject.transform.rotation.y <= 180f)
 		{
 			leftOrRight = 90f;
@@ -39,13 +44,28 @@ public class RangedAttack : MonoBehaviour {
 			leftOrRight = -90f;
 		}
 
-		//If the player pressed the P key & isn't on CoolDown
-		if (Input.GetKeyDown(KeyCode.P)&&CanShoot) {				
+		//If the player pressed the P key & isn't on CoolDown & has ammo, subtract bullet count
+		if (Input.GetKeyDown(KeyCode.P) && CanShoot && bulletCount > 0) {	
 			//Now start a coroutine that will wait before firing.
 			StartCoroutine(WaitAndFire(WaitBeforeFiring));
 			CanShoot=false;
-			StartCoroutine(CoolDown(CooldownTime));			
+			StartCoroutine(CoolDown(CooldownTime));
+
+            bulletCount--;
 		}
+
+        // we don't want the ammo to ever be a negative number
+        if (bulletCount <= 0)
+            bulletCount = 0;
+
+        // When the player runs out of ammo and tries to fire a bullet, play the 'click' no ammo sound
+        if (Input.GetKeyDown(KeyCode.P) && bulletCount <= 0)
+        {
+            GetComponent<AudioSource>().clip = noAmmo;
+            GetComponent<AudioSource>().Play();
+
+            Debug.Log("Out of ammo, sucker! Go find some more!");
+        }
 	}
 
 	IEnumerator WaitAndFire(float waitTime)
@@ -53,7 +73,7 @@ public class RangedAttack : MonoBehaviour {
 		yield return new WaitForSeconds(waitTime);
 		Vector3 spawnPos = Vector3.zero;
 
-		spawnPos = SpawnPosition.position;		
+		spawnPos = SpawnPosition.position;
 
 		//Instantiate mustache on our spawn position.
 		LastFiredProjectile = GameObject.Instantiate(ProjectileObject,spawnPos,Quaternion.AngleAxis(leftOrRight,Vector3.up)) as GameObject;
